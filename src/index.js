c7b6a8a32ec59692a64907c575b6d305244f5309
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-07-09 23:24:21 +0530

    continue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-return <div> Hi there ! </div>;
-};
+
+
+class App extends React.Component {
+
+constructor(props) {
+super(props);
+
+this.state = {lat : null, errorMessage: ' '};
+window.navigator.geolocation.getCurrentPosition(
+position =>{
+this.setState({lat: position.coords.latitude });
+},
+err => {
+this.setState({ errorMessage: err.message});
+}
+);
+}
+
+
+render() {
+if(this.state.errorMessage && !this.state.lat){
+return <div> Error: {this.state.errorMessage} </div>;
+}
+ if( this.state.lat && !this.state.errorMessage ){
+   return <div> Loading ! </div>;
+
+}
+
+return <div> Latitude: {this.state.lat} </div>;
+
+}
+}
+
 
 ReactDOM.render(
 <App/>,
