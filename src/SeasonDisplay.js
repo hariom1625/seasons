1f3e8eeff6e3a32ae4ebd691b6ee4336bb1aeb3c
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-07-21 23:47:04 +0530

    added some css

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+summer: {
+text: "Lets hit the beach !",
+iconName : 'sun'
+}, winter: {
+text: "Burr, it is chilly",
+iconName: 'snowflake'
+}
+
+}
 
 const getSeason = (lat,month ) => {
 if(month>2 && month<9) {
@@ -14,8 +26,16 @@ return lat > 0 ? 'winter' : 'summer';
 
 const SeasonDisplay = (props) => {
 const season = getSeason(props.lat, new Date().getMonth());
-console.log(season);
-return <div> Season Display </div>;
+// const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!';
+// const icon = season=== 'winter' ? 'snowflake' : 'sun';
+const{text, iconName} = seasonConfig[season];
+return (
+ <div className={`season-display ${season}`}>
+<i className={`icon-left massive ${iconName} icon`}></i>
+ <h1> {text} </h1>
+<i className={`icon-right massive ${iconName} icon`}></i>
+</div>
+);
 
 };
 
